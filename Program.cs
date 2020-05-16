1dda2dbe97bbfd55241193d9b2928496b814be13
Elvis Junior Duarte Valario <59875640+elvis201311@users.noreply.github.com>
2020-05-16 15:31:34 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace Imprir_un_Nombre
+namespace Verificar_que_dia_es
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My name is Ronald Alvarado");
+            String dato = " ";
+            int numero;
+
+            Console.WriteLine("Que dia es?\n");
+
+            Console.Write("Digite un numero del 1 al 7: ");
+            dato = Console.ReadLine();
+            numero = Convert.ToInt32(dato);
+
+            switch (numero) {
+                case 1:
+                    Console.WriteLine("\nEs Domingo!");
+                    break;
+                case 2:
+                    Console.WriteLine("\nEs Lunes!");
+                    break;
+                case 3:
+                    Console.WriteLine("\nEs Martes!");
+                    break;
+                case 4:
+                    Console.WriteLine("\nEs Miercoles!");
+                    break;
+                case 5:
+                    Console.WriteLine("\nEs Jueves!");
+                    break;
+                case 6:
+                    Console.WriteLine("\nEs Viernes!");
+                    break;
+                case 7:
+                    Console.WriteLine("\nEs Sabado!");
+                    break;
+            }
+
         }
     }
 }

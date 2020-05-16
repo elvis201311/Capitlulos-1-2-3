a17fc3075205b129e5d80c2f20cd95f19ca6fe59
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-05-16 15:21:58 -0400

    Add project files.

---
+++ Imprimir un Mensaje/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Imprimir_un_Mensaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, i'm mack, nice to meet you!");
+            Console.WriteLine("Hi, i am Roy, Nice to meet you too.");
+        }
+    }
+}
